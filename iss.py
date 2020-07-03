371902c6d61a0ba13fcbf03dbfb7c677948bee0f
Leanne Benson <stylistleannerowe@gmail.com>
2020-07-03 01:40:18 -0400

    indy coordinates +pin

--- iss.py
+++ iss.py
@@ -46,6 +46,26 @@ def iss_risetime(latitude, longitude):
     return time.ctime(passover)
 
 def main():
+    astro_info = astronaut_info()
+    print('Number of astronauts on ISS: {}'.format(len(astro_info)))
+    for astro in astro_info:
+        print(' - {} in {}'.format(astro['name'], astro['craft']))
+
+    latitude, longitude = iss_location()
+    print('Real-time ISS Co-Ordinates: latitude={:.02f} longitude={:.02f}'.format(latitude, longitude)')
+    
+    map = None
+    try:
+        map = iss_map(latitude, longitude)
+        indiana_latitude = 39.791000
+        indiana_longitude = -86.148003
+        location_pin = turtle.Turtle()
+        location_pin.penup()
+        location_pin.color('yellow')
+        location_pin.goto(indiana_latitude, indiana_longitude)
+        location_pin.dot(5)
+        
+
     pass
 
 
