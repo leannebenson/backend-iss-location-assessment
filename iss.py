fc8fe48341a711419ac237e82b2d5da7b85b846e
Leanne Benson <stylistleannerowe@gmail.com>
2020-07-02 22:36:51 -0400

    got astronaut and ISS coordinate info

--- iss.py
+++ iss.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+__author__ = 'LeanneBenson'
+
+import requests
+import time
+import turtle
+
+iss_api = 'http://api.open-notify.org'
+
+def astronaut_info():
+    r = requests.get(iss_api + '/astros.json')
+    r.raise_for_status()
+    return r.json()['people']
+
+def iss_location():
+    r = requests.get(iss_api + '/iss-now.json')
+    r.raise_for_status()
+    position = r.json()['iss_position']
+    latitude = float(position['latitude'])
+    longitude = float(position['longitude'])
+
+    return latitude, longitude
 
 
+    
 def main():
     pass
 
